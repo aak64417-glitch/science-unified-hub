7a3b2eaa98b4b2fd275cc9ee3180bdcad78520d5
aak64417-glitch <aak64417@gmail.com>
2026-09-22 19:54:10 +0300

    Update cache name to science-hub-v5-final

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='science-hub-v4-fast-dashboard';
+const CACHE='science-hub-v5-final';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./journey-student.html','./journey-teacher.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
